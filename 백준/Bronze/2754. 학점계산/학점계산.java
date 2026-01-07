0d8104985923e26fb21d9a651056a8d15c614e97
JIEUN <113418196+Jajaknamu@users.noreply.github.com>
2026-01-07 11:13:45 +0900

    [Bronze III] Title: 학점계산, Time: 172 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.Scanner;
+
+// 백준에서는 이 세팅으로 해야 컴파일 잘됨
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        String grade = sc.nextLine();
+
+        switch (grade) {
+            case "A+" :
+                System.out.println(4.3);
+                break;
+            case "A0" :
+                System.out.println(4.0);
+                break;
+            case "A-" :
+                 System.out.println(3.7);
+                 break;
+            case "B+":
+                System.out.println(3.3);
+                break;
+            case "B0" :
+                System.out.println(3.0);
+                break;
+            case "B-" :
+                System.out.println(2.7);
+                break;
+            case "C+":
+                System.out.println(2.3);
+                break;
+            case "C0" :
+                System.out.println(2.0);
+                break;
+            case "C-" :
+                System.out.println(1.7);
+                break;
+            case "D+":
+                System.out.println(1.3);
+                break;
+            case "D0" :
+                System.out.println(1.0);
+                break;
+            case "D-" :
+                System.out.println(0.7);
+                break;
+            case "F":
+                System.out.println(0.0);
+                break;
+        }
+    }
+}
+
